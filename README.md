637e0b377a6502c1c045cb5b0e6c0090a63b1ae1
wh2017 <hong.wei@daocloud.io>
2017-10-28 22:51:13 +0800

    A  add  Arcade Game Clone_zh

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
+
 # Arcade-Game-Clone_zh
+### Introduction
+**经典街机游戏克隆**是主要利用JavaScript的面向对象、HTML5 Canvas等知识点来编写的一个小游戏，有利于更深一步理解JavaScript中几个常见并且容易混淆的概念：比如*构造函数、原型、原型链*、以及*实例对象*，在编写代码的过程中，你会更深一步的思考他们之间的关系。
+
+### Clone && Run
+* 下载： 
+`git clone`
+* 运行：
+下载文件后，双击index.html文件，或者右击index.html文件 => 打开方式 => 选择任意你安装的浏览器
+
+### Reference
+以下几篇是我在做这个小项目时，寻找的一些不错的文档。我觉得看文档不是去看别人怎么完成的一个小游戏，而是去看别人对待问题的看法、如何分析的，以及解决问题的思路
+
+[Frogger Game: Getting Started](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true)
+
+[如果你不知道如何开始写”经典街机游戏克隆”的话](http://discussions.youdaxue.com/t/topic/32577)
+
+[Classic Arcade Game 经典街机游戏克隆 第一弹 - 个人思路与完整实现](http://discussions.youdaxue.com/t/classic-arcade-game/36088)
+
+### Rules
+* 玩家（player）可通过键盘的up、left、down、right键来控制玩家的上下左右移动，但不许超过Canvas范围
+* 敌人（enemy）已随意速度、随意位置移动，可循环的出现的Canvas上
+* 玩家在穿越石头的过程中，不许碰到敌人，若接触到敌人，则game over,玩家回到初始位置重新开始，当玩家经过石头到达河岸且中途不被敌人触碰到，则为胜利
---
+++ js/app.js
@@ -0,0 +1,99 @@
+// 这是我们的玩家要躲避的敌人 
+var Enemy = function(x, y, speed) {
+    // 要应用到每个敌人的实例的变量写在这里
+    // 我们已经提供了一个来帮助你实现更多
+    this.x = x;
+    this.y = y;
+    this.speed = speed
+    // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// 此为游戏必须的函数，用来更新敌人的位置
+// 参数: dt ，表示时间间隙
+Enemy.prototype.update = function(dt) {
+    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+    // 都是以同样的速度运行的，当敌人移除canvas时，设置再次进入的初始位置为10
+    this.x = this.x > 505 ? 10 : this.x + this.speed * dt;
+};
+
+// 此为游戏必须的函数，用来在屏幕上画出敌人，
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// 自己的玩家类
+var Player = function(x, y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/char-cat-girl.png';
+}
+// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+Player.prototype.update = function() {
+    if (this.y < 0) {
+        // 暂时先提供这种很不友好的提醒方式
+        alert('恭喜您，通关成功！');
+        this.x = 202;
+        this.y = 83 * 3.5;
+    }
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+    switch(key) {
+        case 'left':
+            this.x > 55 ? this.x -= 101 : '';
+            break;
+        case 'up':
+            this.y -= 83;
+            break;
+        case 'right':
+            this.x < 400 ? this.x += 101  : '';
+            break;
+        case 'down':
+            this.y < 300 ? this.y += 83 : '';
+            break;
+        default: 
+            return '';
+    }
+}
+
+// 碰撞检测函数
+Player.prototype.checkCollisions = function() {
+    var that = this;
+    allEnemies.forEach(function(enemy) {
+        if (Math.abs(enemy.x - that.x) < 50 && Math.abs(enemy.y - that.y) < 40) {
+            player.x = 202;
+            player.y = 83 * 3.5;
+            alert('sorry,game over')
+        }
+    });
+}
+
+// 现在实例化所有对象
+// 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
+// 把玩家对象放进一个叫 player 的变量里面
+var allEnemies = [];
+for(var i = 1; i <= 5; i++) {
+    // enemy：x:随机产生，y:随机数产生，尽量控制在每一行石头中间，speed: 随机速度
+    allEnemies.push(new Enemy(Math.random() * 500, 83 * Math.ceil(Math.random() * 3) - 20, Math.random() * 500));
+}
+// 玩家默认位置：x: canvas底部水平居中位置,y:倒数第二行草坪中间
+var playerX = 101 * 2, playerY = 83 * 3.5;
+var player = new Player(playerX, playerY);
+
+// 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
+// 方法里面。
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
